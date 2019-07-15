4aba60da9d74ee59a885ca5a3f859fee64e36457
Josiah <josiahroa18@gmail.com>
2019-07-15 16:37:07 -0600

    Added api key security

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css'
+import APIKEY from './config'
+import './App.css';
 
 class App extends React.Component{
     render(){
         return(
             <div className="App">
-                <h1>Hello World</h1>
-            </div>
+                <h1>Hello</h1>
+                {APIKEY}            
+                </div>
         );
     }
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />,document.getElementById('root'));
